41c6a2cc28c5bf9751d7dc06eb22f67b07d6e714
willk10 <33758468+willk10@users.noreply.github.com>
2017-11-17 11:21:25 -0500

    first upload

---
+++ Madlib_WK.py
@@ -0,0 +1,75 @@
+
+import time
+
+## source http://ordinaryhumanlanguage.ca/blog/wp-content/uploads/2017/07/Mad-Libs-Example.jpg
+
+
+print("Write a noun")
+noun1 = input()
+
+print("Write a person in the room")
+personintheroom1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+print("Write a part of the body {plurl}")
+partofthebody1 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+
+print("Write a noun")
+noun2 = input()
+
+print("Write a noun")
+noun3 = input()
+
+print("Write a plural noun")
+pluralnoun1 = input()
+
+print("Write a type of liquid")
+typeofliquid1 = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write a noun")
+noun4 = input()
+
+print("Write a noun")
+noun5 = input()
+
+print("Write a noun")
+noun6 = input()
+
+print("Write a plural noun")
+pluralnoun2 = input()
+
+print("Write a female in the room")
+femaleintheroom1 = input()
+
+print("Write a noun")
+noun7 = input()
+
+print("Write a part of the body {plural}")
+partofthebody2 = input()
+
+###
+
+print("It was Thanksgiving, and the scent of the succulent roast " + noun1 +
+      " wafted through my house. " )
+print("'" + personintheroom1 + ", it's time to " + verb1 + "! my mother called." )
+print("I cound not wait to get my" + partofthebody1 + "on that" + adjective1 + "Thanksgiving meal.")
+print("My family sat arounf the dining-room" + noun2 + ".")
+print("The table was laid out with every kind of" + noun3 + "imafinable.")
+print("There was a basket of hot buttered" + pluralnoun1 + "and glasses of sparkling" + typeofliquid1)
+print("The" + adjective2 + "turkey sat, steaming, next to a tureen of" + noun4 + "gravy.")
+print("A bowl of ruby-red" + noun5 + "sauce, sweat-"+noun6 + "casserold, and a dish of mashed" + pluralnoun2 "tempted my taste buds.")
+print("But the dish I looked foward to the most was Grandma" + femaleintheroom1 + "'s famous" + noun7)
+print("pie. Thanksgiving is my favorite holiday," + partofthebody2 + "down.")
+
+time.sleep(100)
+
+
